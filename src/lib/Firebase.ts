0aec092163c0015da505e4e36380d4e43ef23d45
cometes <vloslain@gmail.com>
2026-02-02 14:22:49 +0900

    라이브러리 반응형 대응 빌드 완료

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -4,15 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "REMOVED",
-	authDomain: "gray-and-blue.firebaseapp.com",
-	databaseURL:
-		"https://gray-and-blue-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "gray-and-blue",
-	storageBucket: "gray-and-blue.firebasestorage.app",
-	messagingSenderId: "148771683257",
-	appId: "1:148771683257:web:11cb0bc3265193b0a1d468",
-	measurementId: "G-T7XWX9SPLV",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
